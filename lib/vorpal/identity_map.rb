8d8c98a8e01220e7e7d18543442943d3248b5047
Sean Kirby <seank@nulogy.com>
2015-02-16 23:32:35 -0500

    Makes IdentityMap independent of key object's implementation of #hash and #eql?.

--- lib/vorpal/identity_map.rb
+++ lib/vorpal/identity_map.rb
@@ -5,22 +5,30 @@ class IdentityMap
   end
 
   def get(key_object)
-    @entities[key_object]
+    @entities[key(key_object)]
   end
 
   def set(key_object, object)
-    @entities[key_object] = object
+    @entities[key(key_object)] = object
   end
 
   def get_and_set(key_object)
     object = get(key_object)
-    object = yield(key_object) if object.nil?
+    object = yield if object.nil?
     set(key_object, object)
     object
   end
 
   def map(key_objects)
-    key_objects.map { |k| @entities[k] }
+    key_objects.map { |k| @entities[key(k)] }
+  end
+
+  private
+
+  def key(key_object)
+    return nil unless key_object
+    raise "Cannot put entity '#{key_object.inspect}' into IdentityMap without an id." if key_object.id.nil?
+    [key_object.id, key_object.class.name]
   end
 end
 end
---
+++ spec/vorpal/identity_map_spec.rb
@@ -0,0 +1,51 @@
+require 'unit_spec_helper'
+
+require 'vorpal/identity_map'
+
+describe Vorpal::IdentityMap do
+  let(:map) { Vorpal::IdentityMap.new }
+
+  it 'does not rely on the key objects implementation of hash and eql?' do
+    funny1 = build_funny_entity(1)
+    map.set(funny1, 'funny 1')
+
+    funny2 = build_funny_entity(2)
+    map.set(funny2, 'funny 2')
+
+    expect(map.get(funny1)).to eq('funny 1')
+  end
+
+  it 'raises an exception when the key object does not have an id set' do
+    entity = build_entity(nil)
+
+    expect { map.set(entity, 'something') }.to raise_error(/Cannot put entity/)
+  end
+
+  def build_entity(id)
+    entity = Entity.new
+    entity.id = id
+    entity
+  end
+
+  class Entity
+    attr_accessor :id
+  end
+
+  def build_funny_entity(id)
+    funny1 = Funny.new
+    funny1.id = id
+    funny1
+  end
+
+  class Funny
+    attr_accessor :id
+
+    def hash
+      1
+    end
+
+    def eql?(other)
+      true
+    end
+  end
+end
