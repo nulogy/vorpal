1c0b219371b6c823b537a70fa8f6a4b6301230c7
Sean Kirby <seank@nulogy.com>
2015-03-09 10:16:56 -0400

    Moves flattening and grouping of aggregates to own module.

---
+++ lib/vorpal/aggregate_utils.rb
@@ -0,0 +1,37 @@
+require 'vorpal/aggregate_traversal'
+
+module Vorpal
+  module AggregateUtils
+    extend self
+
+    def group_by_type(roots, configs)
+      traversal = AggregateTraversal.new(configs)
+
+      all = roots.flat_map do |root|
+        owned_object_visitor = OwnedObjectVisitor.new
+        traversal.accept(root, owned_object_visitor)
+        owned_object_visitor.owned_objects
+      end
+
+      all.group_by { |obj| configs.config_for(obj.class) }
+    end
+  end
+
+  # @private
+  class OwnedObjectVisitor
+    include AggregateVisitorTemplate
+    attr_reader :owned_objects
+
+    def initialize
+      @owned_objects = []
+    end
+
+    def visit_object(object, config)
+      @owned_objects << object
+    end
+
+    def continue_traversal?(association_config)
+      association_config.owned
+    end
+  end
+end
