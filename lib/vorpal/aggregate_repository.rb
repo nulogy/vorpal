0f7f61f25ba68487c29bfc97b7ae0550a0234cb5
Sean Kirby <seank@nulogy.com>
2015-03-08 22:45:20 -0400

    Moves DbDriver into own file.

--- lib/vorpal/aggregate_repository.rb
+++ lib/vorpal/aggregate_repository.rb
@@ -2,6 +2,7 @@ require 'vorpal/identity_map'
 require 'vorpal/traversal'
 require 'vorpal/db_loader'
 require 'vorpal/naive_db_loader'
+require 'vorpal/db_driver'
 
 module Vorpal
 class AggregateRepository
@@ -263,33 +264,6 @@ class AggregateRepository
   def nil_out_object_ids(objects)
     objects ||= []
     objects.each { |object| object.id = nil }
-  end
-end
-
-module DbDriver
-  extend self
-
-  def save(config, db_objects)
-    db_objects.each do |db_object|
-      db_object.save!
-    end
-  end
-
-  def destroy(config, db_objects)
-    db_objects.each do |db_object|
-      db_object.destroy
-    end
-  end
-
-  def get_primary_keys(config, count)
-    result = ActiveRecord::Base.connection.execute("select nextval('#{sequence_name(config)}') from generate_series(1,#{count});")
-    result.column_values(0).map(&:to_i)
-  end
-
-  private
-
-  def sequence_name(config)
-    "#{config.table_name}_id_seq"
   end
 end
 
